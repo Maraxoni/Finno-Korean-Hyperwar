4ad84c075fd0dad12ef27dd8a718822abc0aec38
Maraxoni <maraxoni16@gmail.com>
2023-10-24 18:42:46 +0200

    Add project files.

---
+++ Finno-Korean Hyperwar/Finno-Korean Hyperwar.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <iostream>
+#include "Main/MainFunctions.h"
+
+using namespace std;
+
+
+int main()
+{
+
+    showMainMenu();
+
+}
---
+++ Finno-Korean Hyperwar/Main/MainFunctions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iostream>
+#include <ncurses.h>
+
+using namespace std;
+
+
+void showMainMenu() {
+    getmaxyx(stdscr, rzedy, kolumny); //1
+}
